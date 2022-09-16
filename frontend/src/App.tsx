ed7bf5c9dfc772ed7b3938a0d3ef5814a87592a1
Wylen Joan Lee <57890826+wylenjoan@users.noreply.github.com>
2022-09-16 13:41:52 +0800

    Add routes for root app and registration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from "react-router-dom";
+import routes from './constants/routes';
+
 
 function App() {
   return (
     <div className="App">
-
+      Hello! The current routes are: <br />
+      <ul>
+        <li>
+          <Link to={routes.ROOT}>Root</Link>
+        </li>
+        <li>
+          <Link to={routes.REGISTRATION}>Registration</Link>
+        </li>
+      </ul>
     </div>
   );
 }
