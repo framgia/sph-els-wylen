c502acb0c0c3d02a311a4262bb523907e57c3e30
Wylen Joan Lee <57890826+wylenjoan@users.noreply.github.com>
2022-09-16 10:07:04 +0800

    Add new line after last line of code

--- backend/app/models.py
+++ backend/app/models.py
@@ -62,3 +62,4 @@ class Answer(models.Model):
   is_correct = models.BooleanField(default=False)
   created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True, auto_now_add=False)
+
