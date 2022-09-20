e1644917e22e2216eeb379ad8b81286e93e3fac5
Wylen Joan Lee <57890826+wylenjoan@users.noreply.github.com>
2022-09-20 16:09:43 +0800

    Revise resgister user view

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,11 +1,18 @@
-from rest_framework import generics
+from django.contrib.auth import authenticate, login, logout
+from rest_framework import generics, serializers, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from .models import *
 from .serializers import *
 
 
-class RegisterUser(generics.CreateAPIView):
-  queryset = AppUser.objects.all()
-  serializer_class = UserSerializer
+class RegisterUser(APIView):
+  def post(self, request, format=None):
+    serializer = UserSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class ListUser(generics.ListAPIView):
