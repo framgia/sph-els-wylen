d5b2a8d958b86d8a269df6288555b3f026f95088
Wylen Joan Lee <57890826+wylenjoan@users.noreply.github.com>
2022-09-20 16:10:34 +0800

    Implement login and logout user

--- backend/app/views.py
+++ backend/app/views.py
@@ -23,3 +23,26 @@ class ListUser(generics.ListAPIView):
 class GetUpdateDeleteUser(generics.RetrieveUpdateDestroyAPIView):
   queryset = AppUser.objects.all()
   serializer_class = UserSerializer
+
+
+class LoginUser(APIView):
+  def post(self, request):
+    email = request.data["email"]
+    password = request.data["password"]
+
+    if email and password:
+      user = authenticate(request=request, username=email, password=password)
+
+      if user is not None:
+        login(request, user)
+        return Response(None, status=status.HTTP_200_OK)
+      else:
+        return Response(None, status=status.HTTP_401_UNAUTHORIZED)
+    else:
+      raise serializers.ValidationError()
+
+
+class LogoutUser(APIView):
+  def post(self, request):
+    logout(request)
+    return Response(None, status=status.HTTP_200_OK)
