dc869f5674309787c4dfc4b04781d288d0a9466d
Wylen Joan Lee <57890826+wylenjoan@users.noreply.github.com>
2022-09-26 17:55:25 +0800

    Create categories page and fetch categories from api

--- frontend/src/apiClient/categoryService.ts
+++ frontend/src/apiClient/categoryService.ts
@@ -6,6 +6,12 @@ const createCategory = async (category: Category) => {
   await axiosClient.post(`${apiUrls.CATEGORY}`, category)
 }
 
+const listCategories = async () => {
+  return await axiosClient.get(`${apiUrls.CATEGORY}`)
+}
+
+
 export {
   createCategory,
+  listCategories,
 }
--- frontend/src/pages/admin/CategoryList.tsx
+++ frontend/src/pages/admin/CategoryList.tsx
@@ -1,6 +1,72 @@
+import axios from "axios";
+import { useEffect, useState } from "react"
+import { Button, Table } from "react-bootstrap"
+import { listCategories } from "../../apiClient/categoryService";
+import Category from "../../interfaces/category"
+
 function CategoryList() {
+
+  const [categories, setCategories] = useState<Category[]>([]);
+
+  useEffect(() => {
+    listAllCategories();
+  }, [])
+
+  async function listAllCategories() {
+    try {
+      const allCategories = (await listCategories()).data;
+      setCategories(allCategories);
+    } catch (err) {
+      if (axios.isAxiosError(err)) {
+        console.log(`${err.request.status} ${err.request.statusText}`)
+      }
+    }
+  }
+
   return (
-    <div>Categories</div>
+    <div className="container pt-5">
+      <h2>Categories</h2>
+      <Table striped>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {categories.map(({ id, title, description }) => (
+            <tr key={id} >
+              <td>{title}</td>
+              <td>{description}</td>
+              <td>
+                <Button
+                  size="sm"
+                  variant="secondary"
+                  className="me-2"
+                >
+                  Add word
+                </Button>
+                <Button
+                  size="sm"
+                  variant="primary"
+                  className="me-2"
+                >
+                  Edit
+                </Button>
+                <Button
+                  size="sm"
+                  variant="danger"
+                >
+                  Delete
+                </Button>
+              </td>
+            </tr>
+          ))}
+
+        </tbody>
+      </Table>
+    </div>
   )
 }
 
