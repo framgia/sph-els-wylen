b59b690417dbe8fba2d83bc21cb6c5af7b056312
Wylen Joan Lee <57890826+wylenjoan@users.noreply.github.com>
2022-09-30 10:17:01 +0800

    Fetch categories from database

--- frontend/src/pages/user/Categories.tsx
+++ frontend/src/pages/user/Categories.tsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import axios from "axios";
+import { useEffect, useState } from "react"
+import { Button, Card, Col, Row } from "react-bootstrap"
+import { listCategories } from "../../apiClient/categoryService";
+import Category from "../../interfaces/category"
 
 function Categories() {
+  const [categories, setCategories] = useState<Category[]>([])
+
+  useEffect(() => {
+    listAllCategories();
+  }, [])
+
+  async function listAllCategories() {
+    try {
+      const allCategories = (await listCategories()).data;
+      setCategories(allCategories);
+    } catch (err) {
+      if (axios.isAxiosError(err)) {
+        console.log(`${err.request.status} ${err.request.statusText}`)
+      }
+    }
+  }
+
   return (
-    <div>Categories</div>
+    <div className="container pt-5">
+      <h2>Categories</h2>
+      <Row xs={1} md={2} xl={4} className="g-4 pt-4">
+        {categories.map(({ id, title, description }) => (
+          <Col key={id}>
+            <Card style={{ width: '18rem' }}>
+              <Card.Body>
+                <Card.Title>{title}</Card.Title>
+                <Card.Text>
+                  {description}
+                </Card.Text>
+                <Button variant="primary">Start lesson</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </div>
   )
 }
 
